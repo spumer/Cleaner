ecb0fcef975c55d7b0aefb842617a24d225f5e07
spumer <spumer-tm@yandex.ru>
2021-04-12 01:43:32 +0500

    chore(version): bump 1.2.0

--- src/smsdk_config.h
+++ src/smsdk_config.h
@@ -40,9 +40,9 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"Console Cleaner"
 #define SMEXT_CONF_DESCRIPTION	"Console warning suppressor"
-#define SMEXT_CONF_VERSION		"1.1.1"
-#define SMEXT_CONF_AUTHOR		"Zephyrus, Accelerator"
-#define SMEXT_CONF_URL			"https://github.com/Accelerator74/Cleaner"
+#define SMEXT_CONF_VERSION		"1.2.0"
+#define SMEXT_CONF_AUTHOR		"spumer, Zephyrus, Accelerator"
+#define SMEXT_CONF_URL			"https://github.com/spumer/Cleaner"
 #define SMEXT_CONF_LOGTAG		"Cleaner"
 #define SMEXT_CONF_LICENSE		"GPLv3"
 #define SMEXT_CONF_DATESTRING	__DATE__
